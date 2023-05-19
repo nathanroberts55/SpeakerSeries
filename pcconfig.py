c40811a2ed155b556187002d1bc4f7502b73f3c5
Nathan Roberts <naroberts@microsoft.com>
2023-05-19 11:22:41 -0400

    Base App Configuration and Files

---
+++ pcconfig.py
@@ -0,0 +1,10 @@
+import pynecone as pc
+
+class SpeakerseriesConfig(pc.Config):
+    pass
+
+config = SpeakerseriesConfig(
+    app_name="SpeakerSeries",
+    db_url="sqlite:///pynecone.db",
+    env=pc.Env.DEV,
+)
