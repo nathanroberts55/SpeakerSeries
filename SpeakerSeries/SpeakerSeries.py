bd7ded70bc52dbb8b6b4a2d95aaa928fb0edde4a
Nathan Roberts <naroberts@microsoft.com>
2023-05-19 13:23:09 -0400

    Small Changes to test

--- SpeakerSeries/SpeakerSeries.py
+++ SpeakerSeries/SpeakerSeries.py
@@ -8,30 +8,46 @@ filename = f"{config.app_name}/{config.app_name}.py"
 
 
 class State(pc.State):
-    """The app state."""
+    title: str = "Speaker Series"
 
-    pass
+
+def SpeakerCard(image_src, name, desc) -> pc.Component:
+    return pc.hstack(
+        # Speaker Image
+        pc.image(src=image_src, width="10em"),
+        # Speaker Details (Vertical Stack):
+        pc.vstack(  # Speaker Name
+            pc.heading(name, size="md"),
+            # Speaker Description
+            pc.text(desc),
+            padding="2.5em",
+        ),
+        width="60%",
+        margin="5em",
+    )
 
 
 def index() -> pc.Component:
     return pc.center(
         pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
-            pc.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": "rgb(107,99,246)",
-                },
+            pc.heading(State.title, font_size="2em"),
+            SpeakerCard(
+                "stock_headshot_1.jpg",
+                "John Smith",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
             ),
-            spacing="1.5em",
-            font_size="2em",
-        ),
-        padding_top="10%",
+            SpeakerCard(
+                "stock_headshot_1.jpg",
+                "Wayne Johnson",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            ),
+            SpeakerCard(
+                "stock_headshot_1.jpg",
+                "Allan White",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            ),
+            padding_top="10%",
+        )
     )
 
 
--- pcconfig.py
+++ pcconfig.py
@@ -4,7 +4,8 @@ class SpeakerseriesConfig(pc.Config):
     pass
 
 config = SpeakerseriesConfig(
-    app_name="SpeakerSeries",
+    app_name="Nate Roberts | Speaker Series",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
+    port=3001
 )
