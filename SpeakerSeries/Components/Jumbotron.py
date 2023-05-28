def09b10343297ad06913adf1d265709e6ffee19
Nate Roberts <45110045+nathanroberts55@users.noreply.github.com>
2023-05-28 18:16:38 -0400

    Created content component and edited other components to fit within borders (#5)

--- SpeakerSeries/Components/Jumbotron.py
+++ SpeakerSeries/Components/Jumbotron.py
@@ -1,12 +1,14 @@
 import pynecone as pc
 
 jumbotron_style = {
-    "width": "100vw",
+    "width": "100%",
     "min_height": "50vh",
     "height": "fit-content",
     "color": "white",
     "background": "rgb(2,0,36)",
     "background": "linear-gradient(324deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)",
+    "border_radius": "2em",
+    "box_shadow": "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
 }
 
 
@@ -23,7 +25,7 @@ def Jumbotron() -> pc.Component:
                 "Welcome to Speaker Series, a website that hosts engaging, informative, inspiring conversations with professionals from the technology sector. Here you can ask questions, listen to their stories, insights, and advice on how they prepared for, entered, survived, and thrived in their careers. Whether you are young or old, a beginner or an expert, a student or a teacher, you will find something valuable and motivating in these presentations. Join us and discover how you can pursue your dreams in the field of technology.",
                 text_align="center",
             ),
-            width="90%",
+            width="60%",
         ),
         style=jumbotron_style,
         center_content=True,
